92de7ac3e1e0c9cecbac74f816fd5e884fc999d9
Keurllana <CristianOrellanaRamirez@outlook.com>
2026-03-21 20:02:59 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,8 +8,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
